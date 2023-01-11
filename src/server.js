a9fbbfed6f66b8ed8fc4f924f69ff2ebe43d372e
Vinicius Holtman <viniholtman28@gmail.com>
2023-01-11 13:22:08 -0300

    Created routes to get and post users

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
-import http from "http"
+import http, { METHODS } from "http"
 
+
+const users = []
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end(JSON.stringify(users))
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      name: "John Doe",
+      email: "johndoe@example.com"
+    })
+
+    return res.end("Created user")
+  }
+  
+  
   return res.end("Hello word")
 })
 

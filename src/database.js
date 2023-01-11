a0ff796bf3de039770eb483d6249e5c708cfabbe
Vinicius Holtman <viniholtman28@gmail.com>
2023-01-11 15:47:35 -0300

    Created file db to persist data

--- src/database.js
+++ src/database.js
@@ -1,6 +1,22 @@
-export class Database {
-  #database = {
+import fs from "node:fs/promises"
 
+const databasePath = new URL('../db.json', import.meta.url)
+
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+    .then(data => {
+      this.#database = JSON.parse(data)
+    })
+    .catch(() => {
+      this.#persist()
+    })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
   select(table) {
@@ -15,6 +31,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }

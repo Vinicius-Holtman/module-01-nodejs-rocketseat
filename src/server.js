8b4e3daf21def0d5dc019e98db1c50ef39f4c5fa
Vinicius Holtman <viniholtman28@gmail.com>
2023-01-11 15:11:14 -0300

    Implemented body in req http

--- src/server.js
+++ src/server.js
@@ -3,22 +3,36 @@ import http, { METHODS } from "http"
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === "GET" && url === "/users") {
     return res.end(JSON.stringify(users))
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
-      name: "John Doe",
-      email: "johndoe@example.com"
+      name,
+      email,
     })
 
     return res.writeHead(201).end()
   }
-  
+
   return res.writeHead(404).end()
 })
 
